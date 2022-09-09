f7a4d31c1d7eac04c35c091a0ad0d1fdcd4da813
Priyanka Singh Thakuri <priyankasinghthakuri57@gmail.com>
2022-09-09 20:29:46 +0545

    update

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <AuthProvider>
       <div className="App">
         <Routes>
-          <Route path="/E-commerce-application" element={<Dashboard />}>
+          <Route path="/e-commerce-application" element={<Dashboard />}>
             <Route path="mobileandtablets" element={<MobileandTablets />} />
             <Route path="computerandlaptops" element={<ComputerAndLaptops />} />
             <Route path="homeappliances" element={<HomeAppliances />} />
